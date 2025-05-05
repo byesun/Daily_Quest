40a41644b1dc816bf1e31ca38eae7796aa8f8c6a
byesun <rkdqudtjs6@naver.com>
2025-05-05 12:27:42 +0900

    feat: 초기 프로젝트 설정

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
+</script>
+
+<template>
+  <div>
+    <div>
+      <Header/>
+    </div>
+  </div>
+  <div>
+    <div>
+      <router-view></router-view>
+    </div>
+  </div>
+  <div>
+    <div>
+      <Footer/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
